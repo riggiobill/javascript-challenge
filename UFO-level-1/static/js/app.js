86c6fa8918215d8d6107cfc6e8b0b4c83d91d235
BillRigg <riggiobill@gmail.com>
2020-11-03 17:15:52 -0500

    Framework for app.js
    Added in the functions I'll need to put the code together - used placeholders and comments in the meantime before fully fleshing them out.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,3 +14,31 @@ function check_info(check) {
 // Call the function
 tableData.forEach(check_info);
 
+
+// Create reference variable for the table body using D3.js
+var tbody = d3.select("#ufo-tbody");
+
+
+
+function createTable(tableData) {
+    // Having the variable for tbody, we can create 
+    // and modify rows of data.
+
+
+}
+
+
+
+
+// Uses D3.js to select the submit button
+var submitButton = d3.select("#filter-btn");
+
+
+// Event listener and in-line function to handle
+// click event on the Submit button
+submitButton.on("click", function() {
+
+
+    // ultimately this in-line function will call createTable using
+    // data filtered with the date collected from the submit button
+});
