b8445c7a3e6eab02a6c7d3cd0801f89a02037ccd
BillRigg <riggiobill@gmail.com>
2020-11-03 16:54:44 -0500

    First Tasks
    Double-checked the data list using a function and a forEach method.

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+
+
+// Create the function
+function check_info(check) {
+    console.log(`${check.city}: ${check.state}`);
+}
+
+
+// Call the function
+tableData.forEach(check_info);
+
